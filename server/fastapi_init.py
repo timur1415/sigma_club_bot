6cb698e8243cad6076c43afda661e208dbd50c0d
Тимур <timurmakssudov@gmail.com>
2025-10-09 23:08:05 +0300

    приступаю к mini app

--- server/fastapi_init.py
+++ server/fastapi_init.py
@@ -1,32 +1,19 @@
-import logging
-from telegram import Update, Bot
-from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
-    ConversationHandler,
-    CommandHandler,
-    CallbackQueryHandler,
-    PicklePersistence,
-)
+from telegram import Update
 
-from config.config import TOKEN, WEBHOOK_URL, TELEGRAM_WEBHOOK_PATH, SECRET_TOKEN
-from handlers.common_handler import start
+from config.config import WEBHOOK_URL, TELEGRAM_WEBHOOK_PATH, SECRET_TOKEN
 
-from config.states import MAIN_MENU
-
-from handlers.common_handler import join, why
-
-from handlers.payment_handler import pay
-
-from fastapi import FastAPI, Request, Response, status
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from handlers.bot_init import create_bot_app
 from config.logger import logger
 
+from server.routes import telegram_router
+
 
 def init_fastapi_app():
     app = FastAPI(lifespan=lifespan)
+    app.include_router(telegram_router) 
     return app
 
 
@@ -45,12 +32,7 @@ async def lifespan(app: FastAPI):
         drop_pending_updates=True,
         secret_token=SECRET_TOKEN,
     )
-    @app.post("/telegram/webhook")
-    async def telegram_webhook(request: Request):
-        data = await request.json()
-        update = Update.de_json(data, bot_app.bot)
-        await bot_app.update_queue.put(update)
-        return Response(status_code=status.HTTP_200_OK)
+    
     logger.info('Приложение инициализированно')
     yield
     # что будет происходить при выходе
@@ -59,7 +41,4 @@ async def lifespan(app: FastAPI):
     finally:
         await bot_app.stop()
         await bot_app.shutdown()
-
-
-
 
